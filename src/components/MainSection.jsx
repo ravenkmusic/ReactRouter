b8c5d74216c4e36f6ca228d1e57608b6d4649bab
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-07 14:03:33 -0500

    changed import statement for blue component

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Blue from "./components/Blue";
+import Blue from "src/components/Blue.jsx";
 import Red from "./components/Red";
 import Home from "./components/Home";
 
@@ -7,10 +7,10 @@ export default function MainSection(){
     return (
     <div id="main-section">
         <Routes>
-        <Route path="/blue" element ={<Blue />} />
-        <Route path="/red" element={<Red />} />
-        <Route path="/" element={<Home />} />          
-    </Routes>
+            <Route path="/blue" element ={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/" element={<Home />} />          
+        </Routes>
     </div>
     )
 }
